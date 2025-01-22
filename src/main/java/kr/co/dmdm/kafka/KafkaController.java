bc24739da3275c63d8b8d6c648358722e61d9818
HSH <victoryline629@naver.com>
2025-01-22 15:37:29 +0900

    카프카 주석

--- src/main/java/kr/co/dmdm/kafka/KafkaController.java
+++ src/main/java/kr/co/dmdm/kafka/KafkaController.java
@@ -1,23 +1,23 @@
-package kr.co.dmdm.kafka;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class KafkaController {
-    private final KafkaProducer producer;
-
-    @PostMapping("/kafka1")
-    public void sendMessage1(@RequestParam String message) {
-        producer.sendMessage1(message);
-    }
-    
-    @PostMapping("/kafka2")
-    public void sendMessage2(@RequestBody String message) {
-        producer.sendMessage2(message);
-    }
-}
+//package kr.co.dmdm.kafka;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class KafkaController {
+//    private final KafkaProducer producer;
+//
+//    @PostMapping("/kafka1")
+//    public void sendMessage1(@RequestParam String message) {
+//        producer.sendMessage1(message);
+//    }
+//
+//    @PostMapping("/kafka2")
+//    public void sendMessage2(@RequestBody String message) {
+//        producer.sendMessage2(message);
+//    }
+//}
