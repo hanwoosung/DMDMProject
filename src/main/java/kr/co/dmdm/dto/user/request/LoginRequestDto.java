d5b971d258f5bb8e6591e2c6323edc209b7fcb82
hanwoosung <yiok79@naver.com>
2025-01-23 17:33:41 +0900

    ã로그인 개발중 1차 완료

---
+++ src/main/java/kr/co/dmdm/dto/user/request/LoginRequestDto.java
@@ -0,0 +1,14 @@
+package kr.co.dmdm.dto.user.request;
+
+/**
+ *packageName    : kr.co.dmdm.dto.user.request
+ * fileName       : LoginRequest
+ * author         : 한우성
+ * date           : 2025-01-23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-23        한우성       최초 생성
+ */public class LoginRequest {
+}
