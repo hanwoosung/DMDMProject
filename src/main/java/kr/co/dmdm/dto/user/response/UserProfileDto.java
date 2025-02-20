8be25b2991862e4eda193339a3f4d25f05fc48af
hanwoosung <yiok79@naver.com>
2025-02-20 19:29:00 +0900

    프로필 수정완료

---
+++ src/main/java/kr/co/dmdm/dto/user/response/UserProfileDto.java
@@ -0,0 +1,14 @@
+package kr.co.dmdm.dto.user.response;
+
+/**
+ *packageName    : kr.co.dmdm.dto.user.response
+ * fileName       : UserProfileDto
+ * author         : 한우성
+ * date           : 2025-02-20
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-02-20        한우성       최초 생성
+ */public class UserProfileDto {
+}
